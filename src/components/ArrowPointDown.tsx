74313c83225673f1e70f03482fb046767c31c615
Marco de CNK <virtualnautilus@gmail.com>
2025-04-17 17:16:22 -0300

    polling slime

--- src/components/ArrowPointDown.tsx
+++ src/components/ArrowPointDown.tsx
@@ -1,30 +1,30 @@
 import { motion } from "motion/react"
+// import { useEffect, useState } from "react"
+
 
 const ArrowPointDown = () => {
 
-    const ArrowPointDownVariant = {
-
-        initial: { 
-            y: -20
-          },
-          animate: {
-            y: 20,
-            repeat: Infinity,
-            yoyo: true,
-            transition: {
-                duration: 2,
-                ease: "none",
-            }
-          }
-    }
 
   return (
-    <motion.svg variants={ArrowPointDownVariant} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-  <g stroke="#1E3E62" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
-    <line x1="12" y1="4" x2="12" y2="16" className="w-5" />
-    <polyline points="7 12 12 20 17 12" />
-  </g>
-</motion.svg>
+    <motion.svg
+      initial={{ y: 0 }}
+      animate={{
+        y: [0, 10, 0],
+        transition: {
+          duration: 2,
+          repeat: Infinity,
+          repeatType: "reverse"
+        }
+      }}
+      width={"24"}
+      height={"24"}
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg">
+      <g stroke="#1E3E62" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round" className="w-5">
+        <line x1="12" y1="4" x2="12" y2="16" className="w-5" />
+        <polyline points="7 12 12 20 17 12" />
+      </g>
+    </motion.svg>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,51 @@
 import ArrowPointDown from '@/components/ArrowPointDown'
 import SvgComponentNavbarSlime from '@/components/SvgComponentNavbarSlime'
 import "@/output.css"
+import gsap from 'gsap'
+import { motion } from 'framer-motion'
 
 const Home = () => {
+    gsap.to("#wave-path", {
+        duration: 3,
+        morphSVG: "M0,50 C100,20 200,80 300,50 L300,100 L0,100 Z",
+        repeat: -1,
+        yoyo: true
+    });
     return (
-        <div className="bg-negro h-screen flex flex-col justify-between relative">
-            <div className="w-full">
-                <SvgComponentNavbarSlime />
-            </div>
-
-            <div className="flex-grow flex items-center justify-center md:top-3/4 .-translate-y-50">
-                <img src='/nautilus1.png' className="bg-negro w-200" />
-            </div>
-
-            <div className="w-full flex items-end justify-between pb-8 px-6">
-                {/* Left SVG */}
-                <div className="flex-shrink-0">
-                    <svg className="w-16 h-16" viewBox="0 0 24 24">
-                        {/* Your SVG content here */}
-                    </svg>
+        <>
+            <div className="bg-negro h-dvh flex flex-col justify-between relative">
+                <div className="w-full">
+                    <SvgComponentNavbarSlime />
                 </div>
 
-                {/* Center content */}
-                <div className="flex flex-col items-center">
-                    <h5 className="text-azul text-lg mb-2">Dig deep into the ocean</h5>
-                    <ArrowPointDown />
+                <div className="flex-grow flex items-center justify-center md:top-3/4 .-translate-y-50">
+                    <img src='/nautilus1.png' className="bg-negro w-200 md:w-300  -translate-y-20 md:-translate-y-30" />
                 </div>
 
-                {/* Empty div to balance layout */}
-                <div className="flex-shrink-0 w-16"></div>
+                <div className="w-full flex items-end justify-between pb-8 px-6">
+                    {/* Left SVG */}
+                    <div className="flex-shrink-0">
+                        <svg className="w-16 h-16" viewBox="0 0 24 24">
+                            {/* Your SVG content here */}
+                        </svg>
+                    </div>
+
+                    {/* Center content */}
+                    <div className="flex flex-col items-center">
+                        <h5 className="text-azul text-lg mb-2">Dig deep into the ocean</h5>
+                        <ArrowPointDown />
+                    </div>
+
+                    {/* Empty div to balance layout */}
+                    <div className="flex-shrink-0 w-16"></div>
+                </div>
             </div>
-        </div>
+            <div className="bg-negro h-dvh flex flex-col justify-between relative">
+                <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" className="wave"><defs></defs><path id="wave-path" d="M0,50 C100,20 200,80 300,50 L300,100 L0,100 Z" /></svg>
+
+                <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" className="wave"><defs></defs><path id="wave-path-two" d="M0,50 C100,20 200,80 300,50 L300,100 L0,100 Z" /></svg>
+            </div>
+        </>
 
 
     )
